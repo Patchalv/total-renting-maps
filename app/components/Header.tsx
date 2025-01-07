f381444361c8221a8fc5d54b9e7568ef4573011b
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2025-01-07 17:21:13 +0100

    Header made responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,15 @@
+import MenuIcon from '@mui/icons-material/Menu';
 import Image from "next/image";
 
 const Header = () => {
 
     return (
         <header className="w-full mb-3">
-            <div className="flex justify-between items-center bg-black text-orange-500 py-2 px-2 lg:px-20">
+            <div className="flex justify-center lg:justify-between items-center bg-black text-orange-500 py-2 px-2 lg:px-20">
                 <p>info@fuelmap.com</p>
-                <p>C/De la Tecnología, 5 P.I Los Olivos (Madrid)</p>
+                <p className="hidden lg:block">C/De la Tecnología, 5 P.I Los Olivos (Madrid)</p>
             </div>
-            <div className="flex items-center px-2 lg:px-20 py-2 gap-8">
+            <div className="hidden lg:flex items-center px-2 lg:px-20 py-2 gap-8">
                 <div className="relative w-[277px] h-[44px]">
                     <Image src="/images/ircongas.png" alt="ircongas" fill />
                 </div>
@@ -29,6 +30,12 @@ const Header = () => {
                         <Image src="/images/user-icon.png" alt="user-icon" fill />
                     </div>
                 </div>
+            </div>
+            <div className="flex w-full h-[52px] justify-between items-center lg:hidden px-4 py-2 gap-4">
+                <div className="relative w-1/3 h-full">
+                    <Image src="/images/ircongas.png" alt="ircongas" fill />
+                </div>
+                <MenuIcon fontSize="large" className="text-orange-500" />
             </div>
         </header>
     );
